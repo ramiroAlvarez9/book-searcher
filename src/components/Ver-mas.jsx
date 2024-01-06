4d6de292dbde5d2591b3f8f7c4c5102708f26944
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2024-01-06 16:01:44 -0300

    fixing ui

--- src/components/Ver-mas.jsx
+++ src/components/Ver-mas.jsx
@@ -1,64 +1,71 @@
-import React from 'react';
-import { Icon } from '@iconify/react';
-import '../assets/styles/global.scss';
-import { Button } from '@mui/material';
-import KeyboardDoubleArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
+import React from "react";
+import { Icon } from "@iconify/react";
+import "../assets/styles/global.scss";
+import { Button } from "@mui/material";
+import KeyboardDoubleArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowLeftOutlined";
 
-
-
-const VerMas = ({ dynamicShowingBook, dynamicHiddingBook, titulo, autor, imagen, sinopsis}) => {
-    /* 
+const VerMas = ({
+  dynamicShowingBook,
+  dynamicHiddingBook,
+  titulo,
+  autor,
+  imagen,
+  sinopsis,
+}) => {
     
-        <h2 className="section__gallery--showing--the--book--title">{titulo}</h2>
-        <h3 className="section__gallery--showing--the--book--sub--title">{autor}</h3>
-    
-    
-    */
+  return (
+    <section
+      className="section__gallery--showing--the--book"
+      style={{
+        transform: `translate(${dynamicShowingBook})`,
+      }}
+    >
+      <div className="section__gallery--showing--the--book--container">
+       
+        <div className="section__gallery--showing--the--book--container--img--buttons-titles">
 
-    return (
+          <div className="button__container">
+            <Button
+              size="small"
+              style={{ fontSize: "50%", marginRight: "5%" }}
+              onClick={dynamicHiddingBook}
+              startIcon={<KeyboardDoubleArrowLeftOutlinedIcon />}
+              disableElevation
+            >
+              Volver a la busqueda
+            </Button>
+          </div>
 
-        <section className="section__gallery--showing--the--book" style={{
-            transform: `translate(${dynamicShowingBook})`,
-            
-        }}>
+          <img
+            className="section__gallery--showing--the--book--img"
+            src={imagen}
+            alt="libro"
+          />
 
-
-            <div className="section__gallery--showing--the--book--container">
-               
-                
-
-                <div className='section__gallery--showing--the--book--container--img--buttons-titles'>
-                <div className='button__container'>
-                <Button 
-                    size='small'
-                    style={{fontSize: '10px'}}
-                    onClick = {dynamicHiddingBook}
-                    startIcon = { < KeyboardDoubleArrowLeftOutlinedIcon /> }
-
-                >Volver a la busqueda</Button>
-                </div>
-                    <img className="section__gallery--showing--the--book--img" src={imagen} alt="pilares" />
-                    <div className='section__gallery--showing--the--book--container--titles'>   
-                        <div id='buttons'>
-                            <Button variant="outlined" size = 'small' >COMPRAR </Button>
-                        </div>
-                    </div>
-                </div>
+          <div className="section__gallery--showing--the--book--container--titles">
+            <div id="buttons">
+              <Button variant="outlined" size="small" disableElevation>
+                COMPRAR
+              </Button>
             </div>
+          </div>
+        </div>
 
-            <div className="section__gallery--showing--the--book--sub--synopsys--container">
+        <div className="section__gallery--showing--the--book--sub--synopsys--container">
+          <h4 className="section__gallery--showing--the--book--sub--synopsys--container--h4">
+            {titulo}
+          </h4>
+          <h3 className="section__gallery--showing--the--book--sub--title">
+            {autor}
+          </h3>
+          <p className="section__gallery--showing--the--book--sub--synopsys--container--p">
+            {sinopsis}
+          </p>
+        </div>
 
-                <h4 className="section__gallery--showing--the--book--sub--synopsys--container--h4">{titulo} </h4>
-                <p className="section__gallery--showing--the--book--sub--synopsys--container--p">  {sinopsis} </p>
-
-            </div>
-
-
-        </section>
-
-
-
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default VerMas;
